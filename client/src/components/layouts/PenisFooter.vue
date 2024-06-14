8a5ab218af7f299283d076fa8b1ed1f1434d91b9
Lu Chaohai <chlu@best-inc.com>
2024-06-14 23:39:24 +0800

    basic client demo

---
+++ client/src/components/layouts/PenisFooter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="penis-footer">
+    <p v-for="(item, index) in footerList" :key="index">
+      {{ item }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, readonly } from "vue";
+
+export default defineComponent({
+  setup() {
+    const footerList = readonly([
+      "关于 | 帮助 | 条款 | 反馈",
+      "Copyright © 2024 Penis Music",
+    ]);
+
+    return { footerList };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/css/var.scss";
+@import "@/assets/css/global.scss";
+
+.penis-footer {
+  width: 100%;
+  height: $footer-height;
+  padding: 20px 0;
+  box-sizing: border-box;
+  font-size: 14px;
+  @include layout(center, center, column);
+}
+
+p {
+  height: 30px;
+}
+</style>
